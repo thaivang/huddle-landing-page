cc7b5a01ffc78b65215d869a087410bb9da08377
Thai Vang <thaivangxd@gmail.com>
2019-05-18 00:12:32 -0700

    add buttons

---
+++ app.js
@@ -0,0 +1,10 @@
+﻿function getButton() {
+    //alert("How are you today Jackie?");
+    //var name = document.getElementsByClassName('btn2');
+    //name.innerHTML = "HELLO";
+    
+    var x = document.getElementsByClassName("text1")
+    x[0].innerHTML = "How are you doing Jacky Chan? :) ";
+}
+
+
